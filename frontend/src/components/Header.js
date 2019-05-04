8ee8d2750c9fb8b25f37899dba0747067e1156db
Melanie Shirk <27696612+melashirk@users.noreply.github.com>
2019-05-04 15:38:14 -0400

    simplifying imports

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import {
+    AppBar,
+    Toolbar,
+    Typography,
+    IconButton,
+    withStyles
+} from '@material-ui/core';
+import { Menu } from '@material-ui/icons';
 
 const styles = {
     root: {
@@ -23,7 +25,7 @@ const Header = ({ classes }) => {
         <AppBar position="static">
             <Toolbar variant="dense">
             <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-                <MenuIcon />
+                <Menu />
             </IconButton>
             <Typography variant="h6" color="inherit">
                 Giving Back
